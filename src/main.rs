6fee1ba0faedd4de32d3ce29854c03d78f6112ed
Phan Huy Hoang <hoangph271@gmail.com>
2024-06-22 18:51:19 +0700

    add "Allow negative" checkbox

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,22 @@
 use iced::{
-    widget::{button, column, text, vertical_space},
-    Alignment, Element, Length, Padding, Size,
+    alignment::Horizontal,
+    widget::{button, checkbox, column, container, row, text, vertical_space},
+    Alignment, Element, Length, Size,
 };
 
-#[derive(Debug, Default)]
+#[derive(Debug)]
 struct Counter {
     value: isize,
+    allow_negative: bool,
+}
+
+impl Default for Counter {
+    fn default() -> Self {
+        Self {
+            value: Default::default(),
+            allow_negative: Default::default(),
+        }
+    }
 }
 
 #[derive(Clone, Debug)]
@@ -13,35 +24,60 @@ enum CounterMessage {
     Increment,
     Decrement,
     Reset,
+    ToggleAllowNegative(bool),
 }
 
 impl Counter {
     fn view(&self) -> Element<CounterMessage> {
-        column![
-            button(text("+").size(25))
-                .width(35)
-                .on_press(CounterMessage::Increment),
-            text(self.value).size(65),
-            button(text("-").size(25))
-                .width(35)
-                .on_press(CounterMessage::Decrement),
-            vertical_space().height(10),
-            button("Reset")
-                .style(button::danger)
-                .on_press(CounterMessage::Reset)
-        ]
-        .align_items(Alignment::Center)
-        .width(Length::Fill)
-        .height(Length::Fill)
-        .padding(Padding::from([10, 0]))
+        container(
+            column![
+                button(text("+").size(25))
+                    .width(35)
+                    .on_press(CounterMessage::Increment),
+                text(self.value).size(65),
+                button(text("-").size(25)).width(35).on_press_maybe(
+                    if !self.allow_negative && self.value <= 0 {
+                        None
+                    } else {
+                        Some(CounterMessage::Decrement)
+                    }
+                ),
+                vertical_space().height(10),
+                container(
+                    row![
+                        button("Reset")
+                            .style(button::danger)
+                            .on_press(CounterMessage::Reset),
+                        checkbox("Allow negative", self.allow_negative)
+                            .on_toggle(CounterMessage::ToggleAllowNegative)
+                    ]
+                    .spacing(10)
+                    .align_items(Alignment::Center)
+                )
+                .align_x(Horizontal::Center)
+                .width(Length::Fill)
+            ]
+            .align_items(Alignment::Center)
+            .height(Length::Fill),
+        )
+        .align_x(Horizontal::Center)
         .into()
     }
 
     fn update(&mut self, message: CounterMessage) {
         match message {
             CounterMessage::Increment => self.value += 1,
-            CounterMessage::Decrement => self.value -= 1,
+            CounterMessage::Decrement => {
+                if self.value > 0 || self.allow_negative {
+                    self.value -= 1
+                } else {
+                    self.value = 0
+                }
+            }
             CounterMessage::Reset => self.value = 0,
+            CounterMessage::ToggleAllowNegative(allow_negative) => {
+                self.allow_negative = allow_negative
+            }
         }
     }
 }
@@ -49,7 +85,7 @@ impl Counter {
 fn main() -> iced::Result {
     iced::application("iced_counter by @sneu", Counter::update, Counter::view)
         .window_size(Size {
-            width: 150.0,
+            width: 240.0,
             height: 240.0,
         })
         .run()
