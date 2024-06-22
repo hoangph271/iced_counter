378046e6259b8e2fd8989b3d996a3ce7b62c7a05
Phan Huy Hoang <hoangph271@gmail.com>
2024-06-23 01:59:50 +0700

    add dark_theme Toggler

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 use iced::{
-    alignment::Horizontal,
-    widget::{button, checkbox, column, container, row, text, vertical_space},
-    Alignment, Element, Length, Size,
+    alignment::{Horizontal, Vertical},
+    widget::{button, checkbox, column, container, row, text, vertical_space, Toggler},
+    Alignment, Element, Length, Size, Theme,
 };
 
 #[derive(Debug)]
 struct Counter {
     value: isize,
     allow_negative: bool,
-}
-
-impl Default for Counter {
-    fn default() -> Self {
-        Self {
-            value: Default::default(),
-            allow_negative: true,
-        }
-    }
+    dark_theme: bool,
 }
 
 #[derive(Clone, Debug)]
@@ -25,24 +17,35 @@ enum CounterMessage {
     Decrement,
     Reset,
     ToggleAllowNegative(bool),
+    ToggleDarkTheme(bool),
 }
 
 impl Counter {
     fn view(&self) -> Element<CounterMessage> {
         container(
             column![
-                button(text("+").size(25))
-                    .width(35)
-                    .on_press(CounterMessage::Increment),
-                text(self.value).size(65),
-                button(text("-").size(25)).width(35).on_press_maybe(
-                    if !self.allow_negative && self.value <= 0 {
-                        None
-                    } else {
-                        Some(CounterMessage::Decrement)
-                    }
-                ),
-                vertical_space().height(10),
+                Toggler::new(
+                    Some("Dark theme".into()),
+                    self.dark_theme,
+                    CounterMessage::ToggleDarkTheme
+                )
+                .width(Length::Shrink),
+                row![
+                    button(text("-").size(25)).width(35).on_press_maybe(
+                        if !self.allow_negative && self.value <= 0 {
+                            None
+                        } else {
+                            Some(CounterMessage::Decrement)
+                        }
+                    ),
+                    text(self.value).size(65),
+                    button(text("+").size(25))
+                        .width(35)
+                        .on_press(CounterMessage::Increment),
+                ]
+                .spacing(12)
+                .align_items(Alignment::Center),
+                vertical_space().height(12),
                 container(
                     row![
                         button("Reset")
@@ -51,7 +54,7 @@ impl Counter {
                         checkbox("Allow negative", self.allow_negative)
                             .on_toggle(CounterMessage::ToggleAllowNegative)
                     ]
-                    .spacing(10)
+                    .spacing(12)
                     .align_items(Alignment::Center)
                 )
                 .align_x(Horizontal::Center)
@@ -61,6 +64,7 @@ impl Counter {
             .height(Length::Fill),
         )
         .align_x(Horizontal::Center)
+        .align_y(Vertical::Center)
         .into()
     }
 
@@ -70,23 +74,35 @@ impl Counter {
             CounterMessage::Decrement => {
                 if self.value > 0 || self.allow_negative {
                     self.value -= 1
-                } else {
-                    self.value = 0
                 }
             }
             CounterMessage::Reset => self.value = 0,
             CounterMessage::ToggleAllowNegative(allow_negative) => {
                 self.allow_negative = allow_negative
             }
+            CounterMessage::ToggleDarkTheme(dark_theme) => self.dark_theme = dark_theme,
         }
     }
 }
 
 fn main() -> iced::Result {
     iced::application("iced_counter by @sneu", Counter::update, Counter::view)
+        .theme(get_theme_from_state)
         .window_size(Size {
             width: 240.0,
             height: 240.0,
         })
-        .run()
+        .run_with(|| Counter {
+            value: Default::default(),
+            allow_negative: true,
+            dark_theme: Theme::default() == Theme::Dark,
+        })
+}
+
+fn get_theme_from_state(state: &Counter) -> Theme {
+    if state.dark_theme {
+        Theme::Dark
+    } else {
+        Theme::Light
+    }
 }
