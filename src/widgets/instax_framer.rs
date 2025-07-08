fca0381d02a3e165860ee158d9bd64e8e1677f8a
Phan Huy Hoang <hoang@lightbeam.ai>
2025-07-08 22:11:11 +0700

    feat - image picking in instax_framer.rs

--- src/widgets/instax_framer.rs
+++ src/widgets/instax_framer.rs
@@ -1,18 +1,24 @@
-use std::path::PathBuf;
-
 use iced::{
-    widget::{button, text},
+    widget::{button, image as iced_image, text},
     Element, Task,
 };
+use image::{DynamicImage, ImageReader};
+use native_dialog::{DialogBuilder, MessageLevel};
+use rfd::FileDialog;
+use std::{borrow::Cow, path::PathBuf};
 
 #[derive(Debug, Default)]
 pub struct InstaxFramer {
     selected_file: Option<PathBuf>,
+    loaded_image: Option<DynamicImage>,
 }
 
 #[derive(Debug, Clone)]
 pub enum InstaxFramerMessage {
     PickImage,
+    ImagePicked(PathBuf),
+    ImageLoadingFailed,
+    ImageLoadingFinished(DynamicImage),
 }
 
 impl InstaxFramer {
@@ -23,16 +29,62 @@ impl InstaxFramer {
     }
 
     pub(crate) fn view(&self) -> Element<'_, InstaxFramerMessage> {
-        let Some(selected_file) = &self.selected_file else {
+        let (Some(selected_file), Some(_)) = (&self.selected_file, &self.loaded_image) else {
             return button(text("Pick an image file"))
                 .on_press(InstaxFramerMessage::PickImage)
                 .into();
         };
 
-        text(format!("{}", selected_file.to_string_lossy())).into()
+        iced_image(selected_file)
+            .width(460)
+            .height(620)
+            .content_fit(iced::ContentFit::Contain)
+            .into()
     }
 
-    pub(crate) fn update(&self, _message: InstaxFramerMessage) -> Task<InstaxFramerMessage> {
-        todo!()
+    pub(crate) fn update(&mut self, message: InstaxFramerMessage) -> Task<InstaxFramerMessage> {
+        match message {
+            InstaxFramerMessage::PickImage => {
+                let file = FileDialog::new()
+                    .add_filter("image", &["jpg", "jpeg", "png"])
+                    .pick_file();
+
+                if let Some(file) = file {
+                    return Task::done(InstaxFramerMessage::ImagePicked(file));
+                }
+            }
+            InstaxFramerMessage::ImagePicked(selected_file) => {
+                self.selected_file = Some(selected_file.clone());
+
+                return Task::future(async {
+                    match ImageReader::open(selected_file) {
+                        Ok(image_reader) => match image_reader.decode() {
+                            Ok(image) => InstaxFramerMessage::ImageLoadingFinished(image),
+                            Err(_) => InstaxFramerMessage::ImageLoadingFailed,
+                        },
+                        Err(_) => InstaxFramerMessage::ImageLoadingFailed,
+                    }
+                });
+            }
+            InstaxFramerMessage::ImageLoadingFailed => {
+                let _ = DialogBuilder::message()
+                    .set_title("Image loading failed...!")
+                    .set_text(format!(
+                        "Failed to load image at {}",
+                        self.selected_file
+                            .as_ref()
+                            .map(|path| path.to_string_lossy())
+                            .unwrap_or(Cow::Borrowed("Unknown")),
+                    ))
+                    .set_level(MessageLevel::Error)
+                    .alert()
+                    .show();
+            }
+            InstaxFramerMessage::ImageLoadingFinished(dynamic_image) => {
+                self.loaded_image = Some(dynamic_image);
+            }
+        }
+
+        Task::none()
     }
 }
